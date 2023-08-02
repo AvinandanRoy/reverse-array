d2431a972633f14686b099be4d86604d143b5888
Avinandan Roy <abhinandanroylipi@gmail.com>
2023-08-02 19:12:24 +0600

    Sample of reversestring is complete

---
+++ reverseString.js
@@ -0,0 +1,8 @@
+function reverseStringElements(str ){
+    for (let i = str.lenght-1 ; i >= 0 ; i--){
+        console.log(str[i]);
+    }
+}
+
+let myString = "Avinandan Roy";
+reverseStringElements(myString);
