4cba3daf028055c5f80277616323832cdadb20fe
Avinandan Roy <abhinandanroylipi@gmail.com>
2023-08-02 19:04:45 +0600

    printing all element in a string

---
+++ printArray.js
@@ -0,0 +1,9 @@
+function printStringElements(str){
+    for(let i = 0 ; i < str.length ; i++){
+        console.log(str[i]);
+    }
+}
+
+let myString = "I am Avinandan Roy .";
+
+printStringElements(myString);
